3d8357c0d1460b844fd8ab3b13774c0b22d17657
Veerendra <kurraveerendrachowdary2012@gmail.com>
2017-11-13 17:22:22 +0530

    removed comments and unused imports

--- src/app/services/link.service.ts
+++ src/app/services/link.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
-import 'rxjs/add/operator/map';
 import { Link } from '../model/Link';
 import { HttpClient } from '@angular/common/http';
 
@@ -8,15 +6,16 @@ import { HttpClient } from '@angular/common/http';
 export class LinkService {
   private url = 'http://localhost:3000/LinkApi/link';
   private headers = new Headers({ 'Content-Type': 'application/json' });
-  constructor(private http: Http, private httpc: HttpClient) { }
+  constructor(private httpc: HttpClient) { }
   getLinks() {
     return new Promise(resolve => {
-    //return this.http.get(this.url, { headers: this.headers }).map(function (response) { return response.json(); });
-   this.httpc.get<Link[]>(this.url).subscribe((data:Link[]) => {resolve(data); }, err => {console.log(err); });
+   this.httpc.get<Link[]>(this.url).subscribe((data: Link[]) => {resolve(data); }, err => {console.log(err); });
     });
   }
   getLink(id: string) {
-    return this.http.get(this.url + '/' +  id, { headers: this.headers}).map(function (response) { return response.json(); });
+    return new Promise(resolve => {
+      this.httpc.get<Link>(this.url + '/' +  id).subscribe((data: Link) => {resolve(data); }, err => {console.log(err); });
+     });
   }
   addLink(link: Link) {
     link.description = link.description ? link.description : '-';
@@ -31,14 +30,10 @@ export class LinkService {
     return new Promise(resolve => {
       this.httpc.put<Link>(this.url + '/' + link._id, link).subscribe((data: Link) => {resolve(data); }, err => {console.log(err); });
      });
-
-    //return this.http.put(this.url + '/' + link._id, link,
-    //{ headers: this.headers }).map(function (response) { return response.json(); });
   }
   deleteLink(id: string) {
     return new Promise(resolve => {
       this.httpc.delete<Link>(this.url + '/' + id).subscribe((data: Link) => {resolve(data); }, err => {console.log(err); });
      });
-    //return this.http.delete(this.url + '/' +  id, { headers: this.headers}).map(function (response) { return response.json(); });
   }
 }
