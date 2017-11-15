363ee480bcd4899fc884787042b067334663d8ea
Veerendra <kurraveerendrachowdary2012@gmail.com>
2017-11-15 18:43:11 +0530

    fixed edit links

--- src/app/components/link/link.component.ts
+++ src/app/components/link/link.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Link } from '../../model/Link';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
+import { LinksPageComponent } from '../../pages/links-page/links-page.component';
 
 @Component({
   selector: 'app-link',
@@ -11,12 +11,11 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class LinkComponent implements OnInit {
 @Input() link: Link;
   constructor(private modalService: NgbModal) { }
-
+  @Output()
+  edit: EventEmitter<string> = new EventEmitter();
   ngOnInit() {
   }
-  editLink(linkModal) {
-    const modalRef = this.modalService.open(linkModal);
-    const link1: Link = this.link;
-    modalRef.componentInstance.link = link1;
+  editLink() {
+    this.edit.emit('edit');
   }
 }
